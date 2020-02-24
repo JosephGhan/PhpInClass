53086bcb5bd257d113b06ae7deccf9fb30c2dfc9
JosephGhan <47993853+JosephGhan@users.noreply.github.com>
2020-02-24 09:46:49 -0600

    lab

--- InClass2-24/index.php
+++ InClass2-24/index.php
@@ -1,6 +1,23 @@
 <?php
 
+    $files = file("products.txt");
 
-
+    $submission = filter_input(INPUT_POST, 'formType');
+    switch($submission)
+    {
+        case("Order");
+            $name = filter_input(INPUT_POST, 'name');
+            $product = filter_input(INPUT_POST, 'product');
+            $quantity = filter_input(INPUT_POST, 'quantity');
+            $fileName = str_replace(' ','',strtolower($name)) . '.txt';
+            $orderInfo = "Name: " . $name . "\nProduct: " . $product . "Quantity: " . $quantity;
+            //echo($orderInfo);
+            file_put_contents($fileName, $orderInfo);
+            include("orderComplete.php");
+            break;
+        default;
+            include("orderForm.php");
+            break;
+    }
 
 ?>
